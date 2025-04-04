34d4cfb8368c22bedf1eb7d5a021399898041f90
Noh Hyeokcheon <makdeng0132@gmail.com>
2025-04-04 10:33:46 +0900

    화면 투명도 및 전체 화면 구현현

--- screen-capture/src/App2.tsx
+++ screen-capture/src/App2.tsx
@@ -10,7 +10,7 @@ function App2() {
     }
 
     const canvasRef = useRef<HTMLCanvasElement>(null);
-    const ctxRef = useRef<CanvasRenderingContext2D | null>(null);
+   
     //현재 그리기 모드인지 여부
     const [isDrawing, setIsDrawing] = useState(false);
     //선택한 사각형 영역을 state 로 관리하기
@@ -21,22 +21,17 @@ function App2() {
         endY : 0
     });
 
-    useEffect(()=>{
-        if(canvasRef.current){
-            const ctx = canvasRef.current.getContext("2d");
-            ctxRef.current = ctx;
-        }
-    },[isDrawing]);
-
+    //마우스 다운 이벤트가 일어났을 때때
     const handleMouseDown = (e: React.MouseEvent) => {
+        //현재 그리고 있는 상태로 변경한다
         setIsDrawing(true);
+        //canvas 내에서의 정확한 상태좌표를 얻어내기 위한 계산 
         const rect = canvasRef.current!.getBoundingClientRect();
-    
         const scaleX = canvasRef.current!.width / rect.width;
         const scaleY = canvasRef.current!.height / rect.height;
-    
         const x = (e.clientX - rect.left) * scaleX;
         const y = (e.clientY - rect.top) * scaleY;
+        // state 에 해다 좌표를 저장한다
         setState({
           startX: x,
           startY: y,
@@ -47,33 +42,41 @@ function App2() {
       };
     
       const handleMouseMove = (e: React.MouseEvent) => {
+        // mouse move 이벤트가 발생하더라도 현재 그리는 중이 아니면 함수 여기서 종료
         if (!isDrawing) return;
+        //현재 마우스의 좌표 얻어내기기
         const rect = canvasRef.current!.getBoundingClientRect();
-    
         const scaleX = canvasRef.current!.width / rect.width;
         const scaleY = canvasRef.current!.height / rect.height;
-    
         const currentX = (e.clientX - rect.left) * scaleX;
         const currentY = (e.clientY - rect.top) * scaleY;
-       
+        //현재 state 에서 출발점의 좌표를 얻어낸다다
         const { startX, startY } = state;
-    
+        
+        //출발점의 좌표와 현재 좌표를 비교해서 더 작은 값을 얻어낸다.
         const x = Math.min(startX, currentX);
         const y = Math.min(startY, currentY);
+        //현재 좌표와 시작점 사이의 거리를 얻어낸다.
         const width = Math.abs(currentX - startX);
         const height = Math.abs(currentY - startY);
-    
-        const ctx = ctxRef.current;
+        //canvas 에 그림을 그릴 도구의 참조값 얻어와서
+        const ctx = canvasRef.current?.getContext("2d");
     
         if (ctx) {
+          // .claearRect(x, y, width, height) 사각형 영역에 그려진 내용을 모두 지우기
           ctx.clearRect(0, 0, canvasStyle.width, canvasStyle.height);
+          // 채우는 색상상(레드, 그린, 블루, 알파)
           ctx.fillStyle = 'rgba(255, 255, 255, 0)';
+          // 채우는 스타일의 사각형 그리기
           ctx.fillRect(x, y, width, height);
+          // 외곽선 색상
           ctx.strokeStyle = 'red';
+          //선의의 굵기
           ctx.lineWidth = 2;
+          //외곽선 그리기
           ctx.strokeRect(x, y, width, height);
         }
-    
+        // 사각형을 그린 후 현재 좌표를 state 에 저장한다.
         setState((prev) => ({
           ...prev,
           endX: currentX,
@@ -83,6 +86,7 @@ function App2() {
       };
     
       const handleMouseUp = async () => {
+        //마우스를 떼었을 때 다시 상태값을 변경해준다
         setIsDrawing(false);
       };
 
